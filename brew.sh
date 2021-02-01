c42b8b6c0d83a3b5f4a02ceb3748f0592b4a81bc
mriddle <mriddle89@gmail.com>
2021-02-01 19:54:39 +0100

    install essentials only

--- brew.sh
+++ brew.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env zsh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew update
 brew upgrade
 
-brew install hub
-brew install ack
-brew install git
-brew install bash
-brew install bash-completion
-
 brew install fzf # Required for gcob function (git branch hotness)
 
-brew install rbenv rbenv-gem-rehash ruby-build
-
-brew install postgres
+brew install rbenv ruby-build
 
 brew cleanup
